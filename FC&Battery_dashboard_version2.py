80218cadd28cc69e234fe40342b359037b0adf4c
Victor1492Alvarez <victorsamuel1492@gmail.com>
2025-06-08 14:46:00 +0200

    Update FC&Battery_dashboard_version2.py

--- FC&Battery_dashboard_version2.py
+++ FC&Battery_dashboard_version2.py
@@ -10,7 +10,7 @@ import os
 
 st.markdown("""
     <div style="display: flex; align-items: center; gap: 10px;">
-        <h2 style="margin: 0; font-size: 2em;">🔋Camping Truck System Dashboard</h2>
+        <h1 style="margin: 0; font-size: 2em;">🔋Camping Truck System Dashboard</h1>
         <img src="https://raw.githubusercontent.com/Victor1492Alvarez/Fuel_Cell-Battery_kpi-dashboard/main/dashboard_logo.png" width="140" style="margin-left: 8px;" />
     </div>
 """, unsafe_allow_html=True)
