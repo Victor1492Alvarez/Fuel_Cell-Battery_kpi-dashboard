545f60de8d5afd3a3fdfdf90da30ff5a786ea145
Victor1492Alvarez <victorsamuel1492@gmail.com>
2025-06-08 21:24:52 +0200

    Update FC&Battery_dashboard_version2.py

--- FC&Battery_dashboard_version2.py
+++ FC&Battery_dashboard_version2.py
@@ -2,6 +2,7 @@ import streamlit as st
 from kpi_calculator import *
 import matplotlib.pyplot as plt
 import pandas as pd
+from kpi_calculator import fuel_cell_efficiency
 from io import BytesIO
 from fpdf import FPDF
 import os
