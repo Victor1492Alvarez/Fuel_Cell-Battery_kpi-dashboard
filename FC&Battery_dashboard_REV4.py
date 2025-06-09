46f8e14dac1fb7adefb6190473c16f7519701626
Victor1492Alvarez <victorsamuel1492@gmail.com>
2025-06-09 17:10:52 +0200

    Update FC&Battery_dashboard_REV4.py

--- FC&Battery_dashboard_REV4.py
+++ FC&Battery_dashboard_REV4.py
@@ -248,8 +248,8 @@ if st.button("📤 Generate PDF Report"):
 
     # Gráficos
     pdf.ln(3)
-    pdf.image("temp_chart.png", x=8, y=170, w=95,h=75)
-    pdf.image("temp_gauge.png", x=95, y=190, w=125, h=60)
+    pdf.image("temp_chart.png", x=5, y=170, w=85,h=75)
+    pdf.image("temp_gauge.png", x=95, y=190, w=115, h=55)
 
     # Gauge interpretation
     pdf.set_xy(10, 250)
