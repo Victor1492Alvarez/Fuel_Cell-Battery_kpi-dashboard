11714c112fc912c191621a9c696db441adda82a1
Victor1492Alvarez <victorsamuel1492@gmail.com>
2025-06-12 20:26:27 +0200

    Update FC&battery_dashboard_REV5.py

--- FC&battery_dashboard_REV5.py
+++ FC&battery_dashboard_REV5.py
@@ -217,12 +217,13 @@ if st.button("Generate PDF Performance Report"):
     pdf.set_font("Arial", size=8)
     pdf.cell(200, 6, "The gauges show key metrics for system autonomy and energy conversion efficiency.", ln=True)
     
-    pdf.ln(18)
+    pdf.ln(16)
     pdf.set_font("Arial", size=8)
     pdf.cell(200, 3, f"Generated on: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}", ln=True)
     pdf.cell(200, 3, "Coder: Victor Alvarez Melendez", ln=True)
     pdf.cell(200, 3, "Master Student in Hydrogen Technology", ln=True)
     pdf.cell(200, 3, "Technische Hochschule Rosenheim - Campus Burghausen", ln=True)
+   
     pdf.ln(2)
     pdf.set_y(-20)
     pdf.cell(200, 4, "Thanks for using our App. Servus and enjoy your camping days in the Alps!.", ln=True)
