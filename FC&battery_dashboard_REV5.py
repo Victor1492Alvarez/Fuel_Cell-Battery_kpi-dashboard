e1660a670470438639347ac687d3a138cd8aced2
Victor1492Alvarez <victorsamuel1492@gmail.com>
2025-06-13 13:05:33 +0200

    Update FC&battery_dashboard_REV5.py

--- FC&battery_dashboard_REV5.py
+++ FC&battery_dashboard_REV5.py
@@ -183,14 +183,14 @@ st.markdown("### 📄 Export Report as PDF")
     img_url = "https://raw.githubusercontent.com/Victor1492Alvarez/Fuel_Cell-Battery_kpi-dashboard/main/wiring_diagram_1.jpg"
     img_path = "/tmp/wiring_diagram_1.jpg"
 
-    try:
-    response = requests.get(img_url)
-    response.raise_for_status()  # Lanza excepción si la descarga falla
-    with open(img_path, "wb") as f:
+        try:
+        response = requests.get(img_url)
+        response.raise_for_status()  # Lanza excepción si la descarga falla
+        with open(img_path, "wb") as f:
         f.write(response.content)
-    except Exception as e:
-    st.error(f"❌ No se pudo descargar la imagen del diagrama: {e}")
-    img_path = None  # Marca que no se pudo usar
+        except Exception as e:
+        st.error(f"❌ No se pudo descargar la imagen del diagrama: {e}")
+        img_path = None  # Marca que no se pudo usar
 
     pdf = FPDF()
     pdf.add_page()
